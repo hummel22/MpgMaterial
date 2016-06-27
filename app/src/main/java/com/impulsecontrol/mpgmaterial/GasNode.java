df3fa7a897ca397304384193233bc20da6e42349
khummel <hummel.kevin@gmail.com>
2016-06-26 17:41:57 -0700

    t0010 - Created MPG Data Object

---
+++ app/src/main/java/com/impulsecontrol/mpgmaterial/GasNode.java
@@ -0,0 +1,19 @@
+package com.impulsecontrol.mpgmaterial;
+
+import android.provider.ContactsContract;
+
+import java.util.Date;
+
+/**
+ * Created by droid on 6/26/16.
+ */
+public class GasNode {
+    public Date date;
+    public Double price_per_gallon;
+    public Double gallons;
+    public Integer mileage;
+    public Boolean full_tank;
+    public Double prius_milage;
+    public Double prius_mpg;
+    public Double prius_ave_speed;
+}
