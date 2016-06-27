c8922d05bd4d201ad86b18e2f3e91d673f7d5781
khummel <hummel.kevin@gmail.com>
2016-06-26 19:53:15 -0700

    t0011 - Add Gas Model Unit Tests

---
+++ app/src/test/java/GasModelUnitTest.java
@@ -0,0 +1,207 @@
+import android.util.Log;
+
+import com.impulsecontrol.mpgmaterial.GasModel;
+import com.impulsecontrol.mpgmaterial.GasNode;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by droid on 6/26/16.
+ */
+public class GasModelUnitTest {
+
+
+
+    @Test
+    public void firstNode() throws Exception {
+
+        GasNode node0 = new GasNode();
+        GasModel model = new GasModel();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+        model.addGasNode(node0);
+        assertEquals(null, model.getMPG());
+    }
+
+    @Test
+    public void SecondtNode() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = true;
+
+        model.addGasNode(node0);
+        model.addGasNode(node1);
+        assertEquals((Double)15.0, model.getMPG());
+    }
+
+    @Test
+    public void thirdsNode() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = true;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = true;
+
+        model.addGasNode(node0);
+        model.addGasNode(node1);
+        model.addGasNode(node2);
+        assertEquals((Double)25.0, model.getMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals((Double)15.0, node1.mpg);
+        assertEquals((Double)25.0, node2.mpg);
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals((Double)15.0, mpgs.get(1));
+        assertEquals((Double)25.0, mpgs.get(2));
+
+    }
+//
+    @Test
+    public void NotFull() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = false;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = true;
+
+        model.addGasNode(node0);
+        model.addGasNode(node1);
+        model.addGasNode(node2);
+        assertEquals((Double)20.0, model.getMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals(null, node1.mpg);
+        assertEquals((Double)20.0, node2.mpg);
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals(null, mpgs.get(1));
+        assertEquals((Double)20.0, mpgs.get(2));
+    }
+
+
+    @Test
+    public void notFullTwoSpace() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = false;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = false;
+
+        GasNode node3 = new GasNode();
+        node3.mileage = 1000;
+        node3.gallons = 10.0;
+        node3.mpg = null;
+        node3.full_tank = true;
+
+        model.addGasNode(node0);
+        model.addGasNode(node1);
+        model.addGasNode(node2);
+        model.addGasNode(node3);
+
+        assertEquals((Double)30.0, model.getMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals(null, node1.mpg);
+        assertEquals(null, node2.mpg);
+        assertEquals((Double)30.0, node3.mpg);
+
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals(null, mpgs.get(1));
+        assertEquals(null, mpgs.get(2));
+        assertEquals((Double)30.0, mpgs.get(3));
+    }
+
+
+    @Test
+    public void incorrectOrder() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = false;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = true;
+
+        model.addGasNode(node1);
+        model.addGasNode(node0);
+        model.addGasNode(node2);
+        assertEquals((Double)20.0, model.getMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals(null, node1.mpg);
+        assertEquals((Double)20.0, node2.mpg);
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals(null, mpgs.get(1));
+        assertEquals((Double)20.0, mpgs.get(2));
+    }
+}
