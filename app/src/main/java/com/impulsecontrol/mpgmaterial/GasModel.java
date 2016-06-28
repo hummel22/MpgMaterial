34784d05c8be6e2f8dbedc6d66aeacc88d418a3b
khummel <hummel.kevin@gmail.com>
2016-06-27 18:15:58 -0700

    t0012 - Created Recalculate Function

--- app/src/main/java/com/impulsecontrol/mpgmaterial/GasModel.java
+++ app/src/main/java/com/impulsecontrol/mpgmaterial/GasModel.java
@@ -14,34 +14,45 @@ public class GasModel {
     private List<GasNode> gas_data = new ArrayList<>();
 
     public void addGasNode(GasNode g) {
+        gas_data.add(g);
         Collections.sort(gas_data, new Comparator<GasNode>() {
             @Override
             public int compare(GasNode g1, GasNode g2) {
                 return g1.mileage - g2.mileage;
             }
         });
-        calculateMPG(gas_data.listIterator(gas_data.size()), g);
-        gas_data.add(g);
+        recalculateMPGs();
+
      }
 
     public void deleteGasNode(GasNode g) {
         gas_data.remove(g);
+        recalculateMPGs();
     }
 
     public void calculateMPG(ListIterator<GasNode> node, GasNode g) {
-        if(node.hasPrevious() && g.full_tank) {
+        if(node.hasPrevious() ) {
             GasNode top = node.previous();
-            if (top.full_tank) {
-                g.mpg = (g.mileage - top.mileage) / (g.gallons);
-            } else {
+            if(g.full_tank) {
+                if (top.full_tank) {
+                    g.mpg = (g.mileage - top.mileage) / (g.gallons);
+                } else {
                     g.gallons += top.gallons;
                     calculateMPG(node, g);
                     g.gallons -= top.gallons;
+                }
             }
+        } 
+    }
+
+    public void recalculateMPGs() {
+        ListIterator<GasNode> iter = gas_data.listIterator(gas_data.size()-1);
+        while(iter != gas_data.listIterator(1) && iter.hasPrevious()) {
+            calculateMPG(iter, gas_data.get(iter.nextIndex()));
         }
     }
 
-    public Double getMPG() {
+    public Double getMostRecentMPG() {
         return gas_data.get(gas_data.size() - 1).mpg;
     }
 
--- app/src/test/java/GasModelUnitTest.java
+++ app/src/test/java/GasModelUnitTest.java
@@ -1,12 +1,9 @@
-import android.util.Log;
-
 import com.impulsecontrol.mpgmaterial.GasModel;
 import com.impulsecontrol.mpgmaterial.GasNode;
 
 import org.junit.Test;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,7 +24,7 @@ public class GasModelUnitTest {
         node0.mpg = null;
         node0.full_tank = true;
         model.addGasNode(node0);
-        assertEquals(null, model.getMPG());
+        assertEquals(null, model.getMostRecentMPG());
     }
 
     @Test
@@ -48,7 +45,7 @@ public class GasModelUnitTest {
 
         model.addGasNode(node0);
         model.addGasNode(node1);
-        assertEquals((Double)15.0, model.getMPG());
+        assertEquals((Double)15.0, model.getMostRecentMPG());
     }
 
     @Test
@@ -76,7 +73,7 @@ public class GasModelUnitTest {
         model.addGasNode(node0);
         model.addGasNode(node1);
         model.addGasNode(node2);
-        assertEquals((Double)25.0, model.getMPG());
+        assertEquals((Double)25.0, model.getMostRecentMPG());
         assertEquals(null, node0.mpg);
         assertEquals((Double)15.0, node1.mpg);
         assertEquals((Double)25.0, node2.mpg);
@@ -112,7 +109,7 @@ public class GasModelUnitTest {
         model.addGasNode(node0);
         model.addGasNode(node1);
         model.addGasNode(node2);
-        assertEquals((Double)20.0, model.getMPG());
+        assertEquals((Double)20.0, model.getMostRecentMPG());
         assertEquals(null, node0.mpg);
         assertEquals(null, node1.mpg);
         assertEquals((Double)20.0, node2.mpg);
@@ -156,7 +153,7 @@ public class GasModelUnitTest {
         model.addGasNode(node2);
         model.addGasNode(node3);
 
-        assertEquals((Double)30.0, model.getMPG());
+        assertEquals((Double)30.0, model.getMostRecentMPG());
         assertEquals(null, node0.mpg);
         assertEquals(null, node1.mpg);
         assertEquals(null, node2.mpg);
@@ -195,7 +192,7 @@ public class GasModelUnitTest {
         model.addGasNode(node1);
         model.addGasNode(node0);
         model.addGasNode(node2);
-        assertEquals((Double)20.0, model.getMPG());
+        assertEquals((Double)20.0, model.getMostRecentMPG());
         assertEquals(null, node0.mpg);
         assertEquals(null, node1.mpg);
         assertEquals((Double)20.0, node2.mpg);
@@ -204,4 +201,97 @@ public class GasModelUnitTest {
         assertEquals(null, mpgs.get(1));
         assertEquals((Double)20.0, mpgs.get(2));
     }
+
+
+
+    @Test
+    public void incorrectOrderAllFull() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = true;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = true;
+
+        model.addGasNode(node1);
+        model.addGasNode(node0);
+        model.addGasNode(node2);
+
+        assertEquals((Double)25.0, model.getMostRecentMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals((Double)15.0, node1.mpg);
+        assertEquals((Double)25.0, node2.mpg);
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals((Double)15.0, mpgs.get(1));
+        assertEquals((Double)25.0, mpgs.get(2));
+    }
+
+
+
+
+    @Test
+    public void RecalculateMPGS() throws Exception {
+        GasModel model = new GasModel();
+
+        GasNode node0 = new GasNode();
+        node0.mileage = 100;
+        node0.gallons = 10.0;
+        node0.mpg = null;
+        node0.full_tank = true;
+
+        GasNode node1 = new GasNode();
+        node1.mileage = 250;
+        node1.gallons = 10.0;
+        node1.mpg = null;
+        node1.full_tank = true;
+
+        GasNode node2 = new GasNode();
+        node2.mileage = 500;
+        node2.gallons = 10.0;
+        node2.mpg = null;
+        node2.full_tank = true;
+
+        model.addGasNode(node0);
+        model.addGasNode(node1);
+        model.addGasNode(node2);
+        assertEquals((Double)25.0, model.getMostRecentMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals((Double)15.0, node1.mpg);
+        assertEquals((Double)25.0, node2.mpg);
+        List<Double> mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals((Double)15.0, mpgs.get(1));
+        assertEquals((Double)25.0, mpgs.get(2));
+
+
+        node2.mileage = 1000;
+        node1.mileage = 500;
+
+
+        model.recalculateMPGs();
+
+        assertEquals((Double)50.0, model.getMostRecentMPG());
+        assertEquals(null, node0.mpg);
+        assertEquals((Double)40.0, node1.mpg);
+        assertEquals((Double)50.0, node2.mpg);
+        mpgs = model. getMPGs();
+        assertEquals(null, mpgs.get(0));
+        assertEquals((Double)40.0, mpgs.get(1));
+        assertEquals((Double)50.0, mpgs.get(2));
+    }
+
 }
